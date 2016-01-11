2eddbd1d31821c37177ffb9e3981e906ee264ff6
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 10:38:50 -0700

    Added link model and setup belongs_to/hasmany to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :links
+
   validates :email, uniqueness: true, email_format: { message: " is not in a valid format" }
   validates :password, length: { minimum: 8, message: "must have a minimum of 8 characters"}
   validates :password_confirmation, presence: true
