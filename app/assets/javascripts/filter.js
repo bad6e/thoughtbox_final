50eba39d341ffd95836959a7739630d4bcccf1ae
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 13:40:14 -0700

    Added filter feature

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#filter").keyup(function(){
+    var filter = $(this).val(), count = 0;
+
+    $(".all-links").each(function(){
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+          $(this).fadeOut();
+      } else {
+        $(this).show();
+        count++;
+      }
+    });
+
+    var numberItems = count;
+    $("#filter-count").text("Number of Comments = "+count);
+  });
+});
