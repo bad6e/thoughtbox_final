1e7d18a24f5b006a77d6a9a01a37683ba18f0b62
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 09:47:35 -0700

    Added routes for creating a session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#delete"
+
   get "/join", to: "users#new"
   post "/join", to: "users#create"
 end
