407fcae92f1290152750bd360c749f91e6106369
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 11:12:49 -0700

    Added form and logic to create new links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,26 @@
 class LinksController < ApplicationController
+
+  def index
+    if current_user.links !=
+      @links = current_user.links
+    end
+  end
+
+  def new
+    @links = Link.new
+  end
+
+  def create
+    @links = current_user.links.new(link_params)
+    if @links.save
+      redirect_to links_path, notice: "Link successfully saved!"
+    else
+      render :new
+    end
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:title, :link)
+    end
 end
