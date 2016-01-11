1f0dc2d5d904863842cad634b96a0b4a95c0c480
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 09:26:24 -0700

    Added user modal validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, email_format: { message: " is not in a valid format" }
+  validates :password, length: { minimum: 8, message: "must have a minimum of 8 characters"}
+  validates :password_confirmation, presence: true
 end
