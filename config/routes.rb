950e348f9edb6498cbd9b3aa1254877a2d9baac2
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 09:57:19 -0700

    Added links index page and route to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  resources :links, only: [:index]
+
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#delete"
