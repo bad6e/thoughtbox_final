16d655bed44dee920cc573f3b765c2469ece779d
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 15:04:33 -0700

    Reformatted tests

--- spec/controllers/api/v1/links_controller_spec.rb
+++ spec/controllers/api/v1/links_controller_spec.rb
@@ -4,15 +4,6 @@ RSpec.describe Api::V1::LinksController, type: :controller do
   before(:each) do
     @link_one   = Link.create(title: "CNN",
                               link: "http://www.cnn.com/")
-
-    # @idea_two   = Idea.create(title: "B",
-    #                           body: "B1")
-
-    # @idea_three = Idea.create(title: "C",
-    #                           body: "C1")
-
-    # @idea_four  = Idea.create(title: "D",
-    #                           body: "D1")
   end
 
   describe "PUT /api/v1//:id" do
@@ -32,34 +23,6 @@ RSpec.describe Api::V1::LinksController, type: :controller do
       link_status = Link.find(@link_one.id).read_status
       expect(link_status).to eq(false)
     end
-
-    # it "updates the quality of an idea on the list" do
-    #   put :update, format: :json, id: @idea_one.id, idea: {quality: 'genius'}
-    #   assert_response :success
-
-    #   idea_title = Idea.find(@idea_one.id).quality
-    #   expect(idea_title).to eq("genius")
-    # end
-
-    # it "rejects the quality of an idea if it is not on the list" do
-    #   put :update, format: :json, id: @idea_one.id, idea: {quality: 'INVALID'}
-    #   assert_response 422
-    #   expect(response_data['quality']).to eq(["INVALID is a not a valid quality!"])
-    # end
-
-    # it "rejects an idea on the idea list with no title" do
-    #   put :update, format: :json, id: @idea_one.id, idea: { title: "", body: "Mom" }
-    #   assert_response 422
-
-    #   expect(response_data['title']).to eq(["can't be blank"])
-    # end
-
-    # it "rejects an idea on the idea list with no body" do
-    #   put :update, format: :json, id: @idea_one.id, idea: { title: "Hi", body: "" }
-    #   assert_response 422
-
-    #   expect(response_data['body']).to eq(["can't be blank"])
-    # end
   end
 end
 
