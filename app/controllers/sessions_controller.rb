6698c7a093430dc0dfd19a822c522e302bfef8d0
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 09:58:26 -0700

    upon log in redirect to links index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:login][:email])
     if @user && @user.authenticate(params[:login][:password])
       session[:user_id] = @user.id
-      redirect_to profile_path
+      redirect_to links_path
     else
       flash.now[:errors] = "Invalid Login"
       render :new
