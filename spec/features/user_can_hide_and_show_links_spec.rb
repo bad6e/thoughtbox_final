0f8682f81038dca043b2fa1ae9954797e62d1efe
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 22:37:11 -0700

    Added test coverage for showing links

---
+++ spec/features/user_can_hide_and_show_links_spec.rb
@@ -0,0 +1,62 @@
+require "rails_helper"
+
+feature "registered user hide/show read links", js: true do
+
+  scenario "user can show only unread links" do
+    user ||= User.create(email: "bret@isabeast.com",
+                         password: "password",
+                         password_confirmation: "password")
+
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    user = User.find_by(email: "bret@isnotmodest.com")
+
+    expect(current_path).to eq(links_path)
+
+    fill_in "link[title]", with: "CNN"
+    fill_in "link[link]", with: "http://www.cnn.com/"
+    click_button "Create a new link"
+
+    expect(page).to have_content("CNN")
+    expect(page).to have_content("http://www.cnn.com/")
+
+    click_button "Mark as Read"
+    click_button "Show Unread Links"
+
+    expect(page).to_not have_content("CNN")
+    expect(page).to_not have_content("http://www.cnn.com/")
+  end
+
+  scenario "user can show only read links" do
+    user ||= User.create(email: "bret@isabeast.com",
+                         password: "password",
+                         password_confirmation: "password")
+
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    user = User.find_by(email: "bret@isnotmodest.com")
+
+    expect(current_path).to eq(links_path)
+
+    fill_in "link[title]", with: "CNN"
+    fill_in "link[link]", with: "http://www.cnn.com/"
+    click_button "Create a new link"
+
+    expect(page).to have_content("CNN")
+    expect(page).to have_content("http://www.cnn.com/")
+
+    click_button "Mark as Read"
+    click_button "Show Read Links"
+
+    expect(page).to have_content("CNN")
+    expect(page).to have_content("http://www.cnn.com/")
+  end
+end
