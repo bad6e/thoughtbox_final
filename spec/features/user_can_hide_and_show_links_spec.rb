1b269970d2eb6a2926e2c0a25137668c26f24165
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 22:51:37 -0700

    Added more test coverage

--- spec/features/user_can_hide_and_show_links_spec.rb
+++ spec/features/user_can_hide_and_show_links_spec.rb
@@ -13,8 +13,6 @@ feature "registered user hide/show read links", js: true do
     fill_in "Password", with: "password"
     click_button "Login"
 
-    user = User.find_by(email: "bret@isnotmodest.com")
-
     expect(current_path).to eq(links_path)
 
     fill_in "link[title]", with: "CNN"
@@ -42,8 +40,6 @@ feature "registered user hide/show read links", js: true do
     fill_in "Password", with: "password"
     click_button "Login"
 
-    user = User.find_by(email: "bret@isnotmodest.com")
-
     expect(current_path).to eq(links_path)
 
     fill_in "link[title]", with: "CNN"
@@ -58,5 +54,46 @@ feature "registered user hide/show read links", js: true do
 
     expect(page).to have_content("CNN")
     expect(page).to have_content("http://www.cnn.com/")
+  end
+
+  scenario "user can do both" do
+    user ||= User.create(email: "bret@isabeast.com",
+                         password: "password",
+                         password_confirmation: "password")
+
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    user = User.find_by(email: "bret@isabeast.com")
+
+    expect(current_path).to eq(links_path)
+
+
+    fill_in "link[title]", with: "CNN"
+    fill_in "link[link]", with: "http://www.cnn.com/"
+    click_button "Create a new link"
+
+    fill_in "link[title]", with: "ABC"
+    fill_in "link[link]", with: "http://abc.go.com/"
+    click_button "Create a new link"
+
+    expect(page).to have_content("CNN")
+    expect(page).to have_content("http://www.cnn.com/")
+
+    expect(page).to have_content("ABC")
+    expect(page).to have_content("http://abc.go.com/")
+
+
+    within "#link-list" do
+      first(:button, "Mark as Read").click
+    end
+
+    click_button "Show Unread Links"
+
+    expect(page).to_not have_content("ABC")
+    expect(page).to have_content("CNN")
   end
 end
