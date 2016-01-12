2eed749a0def4497c9705a5c4f412270f39d14bb
Bret Doucette <doucette.bret@gmail.com>
2016-01-11 22:59:46 -0700

    Added controller api tests

--- spec/controllers/api/v1/links_controller_spec.rb
+++ spec/controllers/api/v1/links_controller_spec.rb
@@ -6,7 +6,20 @@ RSpec.describe Api::V1::LinksController, type: :controller do
                               link: "http://www.cnn.com/")
   end
 
-  describe "PUT /api/v1//:id" do
+  describe "GET /api/v1/links/" do
+
+    it "gets a link on the link list" do
+      get :index ,format: :json
+      expect(response_data.first['title']).to eq(@link_one.title)
+    end
+
+    it "gets all the links on the link list" do
+      get :index ,format: :json
+      expect(response_data.count).to eq(1)
+    end
+  end
+
+  describe "PUT /api/v1/links/:id" do
 
     it "updates a link on the link list" do
       put :update, format: :json, id: @link_one.id, link: { read_status: true }
--- spec/features/user_can_hide_and_show_links_spec.rb
+++ spec/features/user_can_hide_and_show_links_spec.rb
@@ -96,4 +96,34 @@ feature "registered user hide/show read links", js: true do
     expect(page).to_not have_content("ABC")
     expect(page).to have_content("CNN")
   end
+
+  scenario "user can tag links as read" do
+    user ||= User.create(email: "bret@isabeast.com",
+                         password: "password",
+                         password_confirmation: "password")
+
+    visit login_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    user = User.find_by(email: "bret@isabeast.com")
+
+    expect(current_path).to eq(links_path)
+
+
+    fill_in "link[title]", with: "CNN"
+    fill_in "link[link]", with: "http://www.cnn.com/"
+    click_button "Create a new link"
+
+    expect(page).to have_content("CNN")
+    expect(page).to have_content("http://www.cnn.com/")
+
+    within "#link-list" do
+      first(:button, "Mark as Read").click
+    end
+    page.has_css?('line-through')
+    page.has_css?('text-decoration')
+  end
 end
