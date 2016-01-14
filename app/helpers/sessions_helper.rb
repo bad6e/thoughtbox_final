e11a1725dc3efa40ccb95d1c4a245c9a7830e5ba
Bret Doucette <doucette.bret@gmail.com>
2016-01-13 20:56:03 -0700

    Refactored sessions controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def valid(user)
+    user && user.authenticate(params[:login][:password])
+  end
+
+  def log_out
+    session[:user_id] = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
